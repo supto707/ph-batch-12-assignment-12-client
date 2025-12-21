b6b490ea4ae36ec49da76fc6132516570fa2316e
supto707 <kaunia.supto@gmail.com>
2025-12-21 20:08:33 +0600

    404 bg update

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-base-200">
+    <div className="min-h-screen flex items-center justify-center bg-white">
       <div className="text-center">
         <h1 className="text-9xl font-bold text-primary">404</h1>
         <h2 className="text-4xl font-bold mt-4 mb-6">Page Not Found</h2>
